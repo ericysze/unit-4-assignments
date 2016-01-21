e01cff85770766a718b92d2edd07276251753570
ericysze <ericysze@gmail.com>
2016-01-21 18:01:38 -0500

    Completed Question 2

--- HWFrom1-17-16(Lists and Sorts).playground/Contents.swift
+++ HWFrom1-17-16(Lists and Sorts).playground/Contents.swift
@@ -15,7 +15,21 @@ Link: https://docs.google.com/document/d/1XioaEqk6VqUPA-ccQhkqP3eAoDthxYyOM9vSPB
 
 
 
-2) 
+2)
+
+var arr = [
+[1, 2, 3, 4],
+[5, 6, 7, 8],
+[9, 0, 1, 2],
+[3, 4, 5, 6]
+]
+
+for var i = 0; i < arr.count; i++ {
+for var j = arr.count - 1; j >= 0; j-- {
+print(arr[j][i])
+}
+}
+
 
 
 
@@ -24,3 +38,28 @@ Link: https://docs.google.com/document/d/1XioaEqk6VqUPA-ccQhkqP3eAoDthxYyOM9vSPB
 
 
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var arr = [
+//    [1, 2, 3, 4],
+//    [5, 6, 7, 8],
+//    [9, 0, 1, 2],
+//    [3, 4, 5, 6]
+//]
+//
+//for var i = 0; i < arr.count; i++ {
+//    for var j = arr.count - 1; j >= 0; j-- {
+//        print(arr[j][i])
+//    }
+//}
